6204e8ea8168c65dcfc747bfef26b20630fd6df8
Elkin Vega <140770582+elkinvega21@users.noreply.github.com>
2025-08-12 14:04:48 -0500

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1755025487342

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_20afe166._.js",
+  "static/chunks/node_modules_08675787._.js"
+],
+    source: "dynamic"
+});
